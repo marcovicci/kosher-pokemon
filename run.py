cd12e6e5a4351df8aa5246931792f7a33c875912
marcovicci <48192998+marcovicci@users.noreply.github.com>
2022-08-22 15:19:41 +1200

    basic koshermon site done

--- run.py
+++ run.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, g, request, session, redirect, url_for
+import sqlite3
 app = Flask(__name__)
+
+POKEMONDB = 'koshermon.db'
 
 @app.route('/')
 def index():
-    return('<h1>hello world</h1>')
+    db = sqlite3.connect(POKEMONDB)
+    tables = get_all(db)
+    db.close()
+    return render_template('index.html',
+    pokemon=tables['pokemon'])
+
+@app.route('/mon/<params>')
+def single_mon(params):
+    db = sqlite3.connect(POKEMONDB)
+    tables = get_one(params, db)
+    db.close()
+    return render_template('single.html',
+    mon=tables['mon'])
+
+def get_all(db):
+    pokemon = []
+    cur = db.execute('SELECT * FROM pokemon_cleaned')
+    for row in cur:
+        pokemon.append(list(row))
+    return {'pokemon':pokemon}
+
+def get_one(params, db):
+    mon = []
+    cur = db.execute("SELECT * FROM pokemon_cleaned WHERE id=%s" % params)
+    for row in cur:
+        mon.append(list(row))
+    return {'mon':mon}
